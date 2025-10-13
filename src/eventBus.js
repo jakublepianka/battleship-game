aba1bb7fb780c04bd222c0bca5cf7321218e334f
resq-u <jakub12lepianka@wp.pl>
2025-10-13 17:54:07 +0200

    Add unsubscribe function

--- src/eventBus.js
+++ src/eventBus.js
@@ -1,15 +1,18 @@
 export const eventBus = {
-
-    events: {},
-    subscribe(eventName, callback){
-        
-        if(!this.events[eventName]) this.events[eventName] = []; 
-        this.events[eventName].push(callback);
-    },
-    publish(eventName, data){
-        if(this.events[eventName]){
-            this.events[eventName].forEach(callback => callback(data));
-        }
-    },
-
+  events: {},
+  subscribe(eventName, callback) {
+    if (!this.events[eventName]) this.events[eventName] = [];
+    this.events[eventName].push(callback);
+  },
+  unsubscribe(eventName, callback) {
+    if (!this.events[eventName]) return;
+    this.events[eventName] = this.events[eventName].filter(
+      (cb) => cb !== callback,
+    );
+  },
+  publish(eventName, data) {
+    if (this.events[eventName]) {
+      this.events[eventName].forEach((callback) => callback(data));
+    }
+  },
 };
