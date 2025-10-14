03f971fdb2ce9dc4f68901ccc41ae642b1c0aaf3
resq-u <jakub12lepianka@wp.pl>
2025-10-14 14:48:50 +0200

    Add tests for gameboard objects

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,43 +1,85 @@
 /* eslint-disable no-undef */
 import { Gameboard } from "../gameboard.js";
 
-test("place ship on gameboard horizontally", () => {
-  const gameboard = Gameboard();
-  gameboard.placeShip([0, 0], 3, true);
-  const placedShip = gameboard.ships.get("[[0,0],[0,1],[0,2]]").stats;
-  expect(placedShip).toEqual({
-    length: 3,
-    hitsTaken: 0,
-  });
-});
+describe("Gameboard", () => {
 
-test("place ship on gameboard vertically", () => {
-  const gameboard = Gameboard();
-  gameboard.placeShip([0, 0], 4, false);
-  const placedShip = gameboard.ships.get("[[0,0],[1,0],[2,0],[3,0]]").stats;
-  expect(placedShip).toEqual({
-    length: 4,
-    hitsTaken: 0,
-  });
-});
+  const mockGameActions = {
+    receiveAttack: jest.fn(),
+    placeShip: jest.fn()
+  };
 
-test("receive & register an attack", () => {
-  const gameboard = Gameboard();
-  gameboard.placeShip([0, 0], 4, false);
-  gameboard.receiveAttack([3, 0]);
-  expect(gameboard.ships.get("[[0,0],[1,0],[2,0],[3,0]]").stats).toEqual({
-    length: 4,
-    hitsTaken: 1,
-  });
-});
+  test("receiveAttack publishes correct attack info", () => {
+    const gameboard = Gameboard("player1", mockGameActions);
 
-test("report whether all ships are sunk or not", () => {
-  const gameboard = Gameboard();
-  gameboard.placeShip([2,3], 1, false);
-  gameboard.placeShip([5,6], 1, true);
-  gameboard.receiveAttack([2, 3]);
-  gameboard.receiveAttack([5,6]);
-  expect(gameboard.areShipsSunk()).toBe(true);
-  gameboard.placeShip([8,8], 1, true);
-  expect(gameboard.areShipsSunk()).toBe(false);
+    gameboard.receiveAttack([9,9]);
+
+    expect(mockGameActions.receiveAttack).toHaveBeenCalledWith({
+      receiver: "player1",
+      missed: [[9,9]],
+      hit: [],
+      areSunk: true
+    });
+  });
+
+  test("setNewShipPlacement publishes correct gameboard state", () => {
+    const gameboard = Gameboard("player1", mockGameActions);
+
+    gameboard.setNewShipPlacement();
+
+    expect(mockGameActions.placeShip).toHaveBeenCalled();
+    expect(mockGameActions.placeShip).toHaveBeenCalledWith(expect.objectContaining({
+      publishedPlayer: "player1"
+    }));
+
+  });
+
+  // test("placeShip places ships correctly", () => {
+  //   const gameboard = Gameboard();
+  //   gameboard.placeShip([9, 9], 4, false);
+  //   const placedShip = gameboard.ships.get("[[9,9],[10,9],[11,9],[12,9]]");
+  //   expect(placedShip).toBe(undefined);
+  // });
+
+  // test("receiveAttack causes ship to take a hit", () => {
+  //   const gameboard = Gameboard();
+  //   gameboard.placeShip([0, 0], 4, false);
+  //   gameboard.receiveAttack([3, 0]);
+  //   expect(gameboard.ships.get("[[0,0],[1,0],[2,0],[3,0]]").stats).toEqual({
+  //     length: 4,
+  //     hitsTaken: 1,
+  //   });
+  // });
+
+  // test("placeShip on gameboard horizontally", () => {
+  //   const gameboard = Gameboard();
+  //   gameboard.placeShip([9, 9], 3, true);
+  //   const placedShip = gameboard.ships.get("[[9,9],[9,8],[9,7]]").stats;
+  //   expect(placedShip).toEqual({
+  //     length: 3,
+  //     hitsTaken: 0,
+  //   });
+  // });
+
+  // test("placeShip on gameboard vertically", () => {
+  //   const gameboard = Gameboard();
+  //   gameboard.placeShip([9, 9], 4, false);
+  //   const placedShip = gameboard.ships.get("[[9,9],[8,9],[7,9],[6,9]]").stats;
+  //   expect(placedShip).toEqual({
+  //     length: 4,
+  //     hitsTaken: 0,
+  //   });
+  // });
+
+  // test("getOffsetPlacement generates correct offset", () => {
+  //   const gameboard = Gameboard();
+  //   const shipWithOffset = gameboard.getOffsetPlacement([[0,0],[0,1]],[
+  //     [-1, -1],[-1, 0],
+  //     [-1, 1], [0, -1],
+  //     [0, 1],  [1, -1],
+  //     [1, 1],  [1, 0],
+  //   ]);
+  //   expect(shipWithOffset).toContainEqual([-1,-1]);
+  //   expect(shipWithOffset).toContainEqual([1,2]);
+  //   expect(shipWithOffset).toContainEqual([-1,2]);
+  // });
 });
