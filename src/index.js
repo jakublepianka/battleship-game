bd3fc3fb95483aad6891e37020be5527a5df23db
resq-u <jakub12lepianka@wp.pl>
2025-10-16 10:20:21 +0200

    Add layout and game  initialization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import { eventBus } from "./eventBus.js"
-import "./css/normalize.css"
+import { eventBus } from "./eventBus.js";
+import { RenderBaseLayout } from "./UI/RenderBaseLayout.js";
+import { RenderDialog } from "./UI/RenderDialog.js";
+import { GameMediator } from "./Controllers/GameMediator.js";
+import "./css/normalize.css";
+import "./css/layout.css";
+import "./css/dialog.css";
+import "./css/outcome.css";
 
+RenderBaseLayout();
+
+const mediator = GameMediator(eventBus);
+RenderDialog(mediator);
+
+// TODO
+// allow another shot when hit?
+// allow 2 human players
+// computer algorithms
+// drag&drop moving ships
+// track wins/losses
